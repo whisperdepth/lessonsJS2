83bcd2b71469a3b96dcecffebe81e19a2cd4d52f
Roman Marchenko <whisperdepth@gmail.com>
2020-04-12 13:35:40 +0300

    g

---
+++ lesson14/task1/index.js
@@ -0,0 +1,11 @@
+let message = "Just learn it!";
+
+function sendMessage(name) {
+  let message = "Just learn it!";
+  const sender = "Gromcode";
+  console.log(`${name}, ${message} Your ${sender}`);
+}
+
+function setMessage(text) {
+  message = text;
+}
