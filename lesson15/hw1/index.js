2a8fe9728b55553d53619c3e07acb5d2c838b7c4
Roman Marchenko <whisperdepth@gmail.com>
2020-04-13 17:38:02 +0300

    htrht

--- lesson15/hw1/index.js
+++ lesson15/hw1/index.js
@@ -1,14 +1,14 @@
-export function createCalculator () => {
-    let memo = 0;
+export function createCalculator() {
+  let memo = 0;
 
- const add = (num) => (memo += num);
- const decrease = (num) => (memo -= num);
- const reset = () => (memo = 0);
- const getMemo = () => memo;
- return {
-     add,
-     decrease,
-     reset,
-     getMemo,
- }
+  const add = (num) => (memo += num);
+  const decrease = (num) => (memo -= num);
+  const reset = () => (memo = 0);
+  const getMemo = () => memo;
+  return {
+    add,
+    decrease,
+    reset,
+    getMemo,
+  };
 }
