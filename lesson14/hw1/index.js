6d5af106118f9bc696312d961310211a2fa6d513
Roman Marchenko <whisperdepth@gmail.com>
2020-04-13 17:37:19 +0300

    thghg

--- lesson14/hw1/index.js
+++ lesson14/hw1/index.js
@@ -4,14 +4,3 @@ export const add = (num) => (memo += num);
 export const decrease = (num) => (memo -= num);
 export const reset = () => (memo = 0);
 export const getMemo = () => memo;
-
-function findDivCount(a, b, n) {
-  let result = 0;
-  for (let i = a; i <= b; i++) {
-    if (i % n === 0) {
-      result += 1;
-    }
-  }
-  return result;
-}
-console.log(findDivCount(1, 10, 3));
