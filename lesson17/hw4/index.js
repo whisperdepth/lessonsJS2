e36360373d255c46965369b3ea464f2e7573349b
Roman Marchenko <whisperdepth@gmail.com>
2020-04-15 19:34:32 +0300

    gffg

--- lesson17/hw4/index.js
+++ lesson17/hw4/index.js
@@ -1,11 +1,20 @@
-export const user = {
+let user = {
   firstName: "John",
   lastName: "Doe",
   getFullName() {
     return `${this.firstName} ${this.lastName}`;
   },
-  setFullName(firstName, lastName) {
+  /*  setFullName(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
+  }, */
+
+  setFullName(fullName) {
+    this.firstName = fullName.split(" ")[0];
+    this.lastName = fullName.split(" ")[1];
   },
 };
+
+/* console.log(user.getFullName());
+user.setFullName("Tom Sayer");
+console.log(user.getFullName());*/
