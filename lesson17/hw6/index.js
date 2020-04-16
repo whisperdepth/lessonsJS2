c0c1b714ee265c152c64975ebf494a44148fdd27
Roman Marchenko <whisperdepth@gmail.com>
2020-04-16 22:38:43 +0300

    gvfbdd

---
+++ lesson17/hw6/index.js
@@ -0,0 +1,37 @@
+export function calculator(a, b) {
+  switch (this.operation) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "/":
+      return a / b;
+    case "*":
+      return a * b;
+    default:
+      return NaN;
+  }
+}
+
+/*
+ * Ф-ция multiplier должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 2 числа и возвращать из произведение
+ */
+calculator.operation = "*";
+export const multiplier = calculator.bind(calculator);
+/*
+ * Ф-ция summator должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 2 числа и возвращать из сумму
+ */
+calculator.operation = "+";
+export const summator = calculator.bind(calculator);
+
+/*
+ * Ф-ция twice должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 1 число и возвращать это число умноженное на 2
+ */
+calculator.operation = "*";
+export const twice = calculator.bind(calculator, (calculator.b = 2));
