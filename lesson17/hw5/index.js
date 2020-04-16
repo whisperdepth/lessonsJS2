9da750a79eb1cee4bd8d353eaa110dbab3defa25
Roman Marchenko <whisperdepth@gmail.com>
2020-04-16 15:06:11 +0300

    ghgfhfg

---
+++ lesson17/hw5/index.js
@@ -0,0 +1,27 @@
+export const timer = {
+  secondsPassed: 0,
+  minsPassed: 0,
+  startTimer() {
+    setInterval(() => {
+      this.secondsPassed += 5;
+      if (this.secondsPassed == 60) {
+        this.minsPassed += 1;
+        this.secondsPassed = 0;
+      }
+    }, 150);
+  },
+
+  getTime() {
+    if (this.secondsPassed > 9)
+      return `${this.minsPassed}:${this.secondsPassed}`;
+    return `${this.minsPassed}:0${this.secondsPassed}`;
+  },
+
+  stopTimer() {
+    clearInterval(this.startTimer);
+  },
+};
+
+timer.startTimer();
+timer.stopTimer();
+setTimeout(() => console.log(timer.getTime()), 2000);
