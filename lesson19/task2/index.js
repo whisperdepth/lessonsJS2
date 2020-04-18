b555638fe61b4a809a8d2cbf5158d0f00732c486
Roman Marchenko <whisperdepth@gmail.com>
2020-04-19 00:40:17 +0300

    e

--- lesson19/task2/index.js
+++ lesson19/task2/index.js
@@ -1,7 +1,20 @@
 export function getOwnProps(obj) {
-    let arr = []
-  if(obj.hasOwnProperty(prop)){
+  let arr = [];
+  for (let prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
       arr.push(prop);
+    }
   }
   return arr;
 }
+
+const user = {
+  name: "Tom",
+  age: 27,
+  sayHi() {
+    console.log("hi");
+  },
+};
+
+let result = getOwnProps(user);
+console.log(result);
