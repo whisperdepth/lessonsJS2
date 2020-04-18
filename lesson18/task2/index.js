f9e0d89ee90e84118b6f93825a00ffd187014b32
Roman Marchenko <whisperdepth@gmail.com>
2020-04-18 12:36:03 +0300

    h

--- lesson18/task2/index.js
+++ lesson18/task2/index.js
@@ -2,7 +2,7 @@ export const wallet = {
     transactions: [1, 5, 89, 337],
     getMax(){
         return Math.max(...this.transactions);
-    }
+    },
     getMin(){
         return Math.min(...this.transactions);
     }
