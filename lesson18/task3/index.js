bc004c570cb6f129881239cf0fb391f1993ae73e
Roman Marchenko <whisperdepth@gmail.com>
2020-04-18 12:57:49 +0300

    f

--- lesson18/task3/index.js
+++ lesson18/task3/index.js
@@ -1,4 +1,4 @@
 export function sumOfSquares() {
-  return [...arguments].reduce((acc, elem) => acc + elem, 0);
+  return [...arguments].reduce((acc, elem) => acc + elem * elem, 0);
 }
 console.log(sumOfSquares(3, 5));
