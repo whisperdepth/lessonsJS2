83140551358a60fb64a2e2531b5de6171fedb0df
Roman Marchenko <whisperdepth@gmail.com>
2020-04-19 00:03:21 +0300

    gggedgd

--- lesson19/task1/index.js
+++ lesson19/task1/index.js
@@ -1,15 +1,24 @@
-function defer(func, ms) {
-  return function () {
-    setTimeout(() => func.apply(this, arguments), ms);
-  };
-}
-
-const user = {
-  name: "Tom",
-  sayHI() {
-    console.log(`Hi, I am ${this.name}`);
+export const vehicle = {
+  name: "Argo",
+  move() {
+    console.log(`${this.name} is moving`);
+  },
+  stop() {
+    console.log(`${this.name} stopped`);
   },
 };
 
-const deferredHi = defer(user.sayHI, 1000);
-deferredHi();
+export const ship = {
+  startMachine() {
+    console.log(`${this.name} lifting anchor up`);
+    this.move();
+  },
+
+  stopMachine() {
+    this.stop();
+    console.log(`${this.name} lifting anchor down`);
+    
+  },
+
+  __proto__: vehicle,
+};
