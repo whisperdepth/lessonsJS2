50af15a8eddd01a2f25beaf68293d723c936a18d
Roman Marchenko <whisperdepth@gmail.com>
2020-04-19 15:34:13 +0300

    1

---
+++ lesson20/task1/index.js
@@ -0,0 +1,25 @@
+export class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  static createEmpty() {
+    return new User();
+  }
+
+  sayHi() {
+    console.log(`Hi, I am ${this.name}`);
+  }
+
+  requestNewPhoto() {
+    console.log(`New photo request was sent for ${this.name}`);
+  }
+
+  setAge(age) {
+    if (age < 0) return false;
+    this.age = age;
+    if (age > 24) this.requestNewPhoto();
+    return age;
+  }
+}
