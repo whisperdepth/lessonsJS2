35e248b381054afbfb7ca9e97259fc0a50f61f18
Roman Marchenko <whisperdepth@gmail.com>
2020-04-20 12:05:25 +0300

    f

---
+++ lesson20/task2/index.js
@@ -0,0 +1,28 @@
+class Vehicle {
+  constructor(name, numberOfWheels) {
+    this.name = name;
+    this.numberOfWheels = numberOfWheels;
+  }
+  move() {
+    console.log(`${this.name} is moving`);
+  }
+
+  stop() {
+    console.log(`${this.name} stopped`);
+  }
+}
+
+class Ship extends Vehicle {
+  constructor(name, numberOfWheels, maxSpeed) {
+    super(name, numberOfWheels);
+    this.maxSpeed = maxSpeed;
+  }
+  move() {
+    console.log(`${this.name} lifting anchor up`);
+    super.move();
+  }
+  sstop() {
+    super.stop();
+    console.log(`${this.name} lifting anchor down`);
+  }
+}
