54caba97ffa927aafec9e74d5f4ade110a54bb25
Roman Marchenko <whisperdepth@gmail.com>
2020-04-21 21:22:24 +0300

    d

---
+++ lesson21/hw11/index.js
@@ -0,0 +1,13 @@
+export function squaredNumbers() {
+  const numbersElem1 = document.querySelector(".number");
+  const dataNum1 = numbersElem1.dataset.number;
+  numbersElem1.dataset.squaredNumber = dataNum1 * dataNum1;
+
+  const numbersElem2 = document.querySelector(".number").nextElementSibling;
+  const dataNum2 = numbersElem2.dataset.number;
+  numbersElem2.dataset.squaredNumber = dataNum2 * dataNum2;
+
+  const numbersElem3 = document.querySelector(".list").lastElementChild;
+  const dataNum3 = numbersElem3.dataset.number;
+  numbersElem3.dataset.squaredNumber = dataNum3 * dataNum3;
+}
