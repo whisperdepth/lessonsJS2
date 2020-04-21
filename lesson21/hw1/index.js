e17123921c094e414fee83562f79d084e7603c15
Roman Marchenko <whisperdepth@gmail.com>
2020-04-21 14:52:38 +0300

    s

---
+++ lesson21/hw1/index.js
@@ -0,0 +1,14 @@
+export function getTitleElement() {
+  const titleElem = document.querySelector(".title");
+  console.dir(titleElem);
+  return titleElem;
+}
+
+getTitleElement();
+
+export function getInputElement() {
+  const inputElem = document.querySelector("input");
+  console.dir(inputElem);
+  return inputElem;
+}
+getInputElement();
