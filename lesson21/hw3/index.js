059944c3fc8d0d1c7f12bb14a0e5fb217f35c2cf
Roman Marchenko <whisperdepth@gmail.com>
2020-04-21 16:02:02 +0300

    l

--- lesson21/hw3/index.js
+++ lesson21/hw3/index.js
@@ -3,25 +3,21 @@ export function getTitle() {
   return titleElem;
 }
 
-getTitle();
 
 export function getDescription() {
   const descElem = document.querySelector(".about").innerText;
   return descElem;
 }
 
-getDescription();
 
 export function getPlans() {
   const plansElem = document.querySelector(".plans").innerHTML;
   return plansElem;
 }
 
-getPlans();
 
 export function getGoal() {
   const goalElem = document.querySelector(".goal").outerHTML;
   return goalElem;
 }
 
-getGoal();
