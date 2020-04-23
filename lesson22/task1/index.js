5c41a7ef3e37a5a3b574ce05fc1d6cb189e86e32
Roman Marchenko <whisperdepth@gmail.com>
2020-04-23 15:29:34 +0300

    а

---
+++ lesson22/task1/index.js
@@ -0,0 +1,39 @@
+const divElem = document.querySelector(".rect_div");
+const pElem = document.querySelector(".rect_p");
+const spanElem = document.querySelector(".rect_span");
+const eventsListElem = document.querySelector(".events-list");
+
+const logTarget = (text, color) => {
+  eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px">${text}</span>`;
+};
+
+const logGreenDiv = logTarget.bind(null, "DIV", "green");
+const logGreenP = logTarget.bind(null, "P", "green");
+const logGreenSpan = logTarget.bind(null, "SPAN", "green");
+
+const logGreyDiv = logTarget.bind(null, "DIV", "grey");
+const logGreyP = logTarget.bind(null, "P", "grey");
+const logGreySpan = logTarget.bind(null, "SPAN", "grey");
+
+divElem.addEventListener("click", logGreyDiv, { capture: true });
+divElem.addEventListener("click", logGreenDiv);
+
+pElem.addEventListener("click", logGreyP, { capture: true });
+pElem.addEventListener("click", logGreenP);
+
+spanElem.addEventListener("click", logGreySpan, { capture: true });
+spanElem.addEventListener("click", logGreenSpan);
+
+const btnClear = document.querySelector(".clear-btn");
+const clearEventsList = () => {
+  eventsListElem.innerHTML = null;
+};
+btnClear.addEventListener("click", clearEventsList);
+
+/* const rectangles = document.querySelector(".rect"); */
+const btnRemoveHandleds = document.querySelector(".remove-handlers-btn");
+const disableHandleds = () => {
+    divElem.removeEventListener("click", logGreyDiv);
+};
+
+btnRemoveHandleds.addEventListener("click", disableHandleds);
