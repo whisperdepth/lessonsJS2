e63f2abf0c8949a07331b131831d3545121a3076
Roman Marchenko <whisperdepth@gmail.com>
2020-04-24 15:49:42 +0300

    f

--- lesson22/hw10/index.js
+++ lesson22/hw10/index.js
@@ -5,10 +5,9 @@ const tasks = [
   { text: "Visit doctor", done: true },
   { text: "Buy meat", done: true },
 ];
+const listElem = document.querySelector(".list");
 
 const renderListItems = (listItems) => {
-  const listElem = document.querySelector(".list");
-
   const listItemsElems = listItems
     .sort((a, b) => a.done - b.done)
     .map(({ text, done }) => {
@@ -31,15 +30,46 @@ const renderListItems = (listItems) => {
 
 renderListItems(tasks);
 
-const listItemElems = document.querySelectorAll(".list__item");
+const lisItemElems = document.querySelectorAll(".list__item");
 const checkBoxElems = document.querySelectorAll(".list__item-checkbox");
-console.log(listItemElems);
 
 const swithCheckBox = (event) => {
-    if (event.target.checked) return event.target.closest(".list__item").classList.add("list__item_done");
-    event.target.closest(".list__item").classList.remove("list__item_done");
+  const sorting = () =>
+    lisItemElems.forEach((lisItemElem) => {
+      if (lisItemElem.classList.contains("list__item_done")) {
+        lisItemElem.remove();
+        listElem.append(lisItemElem);
+      }
+    });
+
+  if (event.target.checked) {
+    event.target.closest(".list__item").classList.add("list__item_done");
+    sorting();
+    return;
+  }
+  event.target.closest(".list__item").classList.remove("list__item_done");
+  sorting();
 };
 
 checkBoxElems.forEach((listItemElem) => {
   listItemElem.addEventListener("change", swithCheckBox);
 });
+
+const createBtnElem = document.querySelector(".create-task-btn");
+const createTask = () => {
+  const listItemElem = document.createElement("li");
+  listItemElem.classList.add("list__item");
+
+  const checkBoxElem = document.createElement("input");
+  checkBoxElem.setAttribute("type", "checkbox");
+  checkBoxElem.classList.add("list__item-checkbox");
+
+  const inputElem = document.querySelector(".task-input");
+  if (inputElem.value == "") return;
+  listItemElem.append(checkBoxElem, inputElem.value);
+  inputElem.value = "";
+  listElem.prepend(listItemElem);
+  listItemElem.addEventListener("change", swithCheckBox);
+};
+
+createBtnElem.addEventListener("click", createTask);
