f41aa08b6e67af9b6f55454afade1f4db5b93c63
Roman Marchenko <whisperdepth@gmail.com>
2020-04-25 23:04:35 +0300

    мам

--- lesson22/hw10/index.js
+++ lesson22/hw10/index.js
@@ -1,7 +1,7 @@
 const tasks = [
-  { text: "Buy milk", done: false },
-  { text: "Pick up Tom from airport", done: false },
-  { text: "Visit party", done: false },
+  { text: "Buy milk", done: true },
+  { text: "Pick up Tom from airport", done: true },
+  { text: "Visit party", done: true },
   { text: "Visit doctor", done: true },
   { text: "Buy meat", done: true },
 ];
