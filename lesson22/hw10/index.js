dda0766517f473c42281d12811d9a0411c046725
Roman Marchenko <whisperdepth@gmail.com>
2020-04-25 22:53:12 +0300

    ммаа

--- lesson22/hw10/index.js
+++ lesson22/hw10/index.js
@@ -3,7 +3,7 @@ const tasks = [
   { text: "Pick up Tom from airport", done: false },
   { text: "Visit party", done: false },
   { text: "Visit doctor", done: true },
-  { text: "Visit meat", done: true },
+  { text: "Buy meat", done: true },
 ];
 
 const renderListItems = (listItems) => {
