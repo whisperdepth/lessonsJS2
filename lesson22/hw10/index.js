1bfc9fa85fc547b32bfa634e77c99f62cc85bb85
Roman Marchenko <whisperdepth@gmail.com>
2020-04-25 10:11:25 +0300

    f

--- lesson22/hw10/index.js
+++ lesson22/hw10/index.js
@@ -53,7 +53,7 @@ const createBtnElem = document.querySelector(".create-task-btn");
 const crealeListItem = () => {
   const inputElem = document.querySelector(".task-input");
   if (inputElem.value == "") return;
-  tasks.unshift({ text: `${inputElem.value}`, done: false });
+  tasks.push({ text: `${inputElem.value}`, done: false });
   inputElem.value = null;
   listElem.innerHTML = null;
   renderListItems(tasks);
