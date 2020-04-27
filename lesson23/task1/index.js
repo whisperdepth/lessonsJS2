ba87e9f49c06ec1947b9e1ed170b44a9bfaac7db
Roman Marchenko <whisperdepth@gmail.com>
2020-04-27 18:29:08 +0300

    d

--- lesson23/task1/index.js
+++ lesson23/task1/index.js
@@ -1,25 +1,49 @@
+const emailInputElem = document.querySelector("#email");
+const passwordInputElem = document.querySelector("#password");
 
+const emailErrorElem = document.querySelector(".error-text_email");
+const passwordErrorElem = document.querySelector(".error-text_password");
 
+const isRequred = (value) => (value ? undefined : "Required");
+const isEmail = (value) =>
+  value.includes("@") ? undefined : "Should be an email";
 
+const validatorsByField = {
+  email: [isRequred, isEmail],
+  password: [isRequred],
+};
 
+const validate = (fieldName, value) => {
+  const validators = validatorsByField[fieldName];
+  return validators
+    .map((validator) => validator(value))
+    .filter((errorText) => errorText)
+    .join(", ");
+};
 
+const onEmailChange = (e) => {
+  const errorText = validate("email", e.target.value);
+  emailErrorElem.textContent = errorText;
+};
 
+const onPasswordChange = (e) => {
+  const errorText = validate("password", e.target.value);
+  passwordErrorElem.textContent = errorText;
+};
 
+emailInputElem.addEventListener("input", onEmailChange);
+passwordInputElem.addEventListener("input", onPasswordChange);
 
+const formElem = document.querySelector(".login-form");
 
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const formData = [...new FormData(formElem)].reduce(
+    (acc, [field, value]) => ({ ...acc, [field]: value }),
+    {}
+  );
 
-// так можно получить данные формы
-const formFields = [...new FormData(formElem)];
-// formFields => [["email", "значение поля email"], ["password", "значение поля password"]]
-const formData = formFields.reduce(function (acc, formField) {
-  const prop = formField[0]; // здесь "name" инпута
-  const value = formField[0]; // здесь "value" инпута
-  // если использовать деструктуризацию, то можно предыдущие 2 строки записать короче
-  // const [prop, value] = formField;
-  return {
-    // используем оператор расширения, чтобы в acc добвить свойства все предыдущих итераций
-    ...acc,
-    // используем вычислимое свойство объекта, чтобы добавить в аккумулятор новое свойство
-    [prop]: value,
-  };
-}, {});
+  alert(JSON.stringify(formData));
+};
+
+formElem.addEventListener("submit", onFormSubmit);
