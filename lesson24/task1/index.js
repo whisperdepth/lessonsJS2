e5a30ee58c875512337dec58505ee277e0462da4
Roman Marchenko <whisperdepth@gmail.com>
2020-04-28 21:00:42 +0300

    f

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,9 +1,5 @@
-const week = ["Mo", "Tu", "We", "Th", "Fr", "St", "Su"];
-
 export const dayOfWeek = (date, days) => {
+  const week = ["Mo", "Tu", "We", "Th", "Fr", "St", "Su"];
   const dateInFuture = new Date(date).setDate(new Date(date).getDate() + days);
   return week[new Date(dateInFuture).getDay()];
 };
-
-const result = dayOfWeek(new Date(2019, 0, 1), 14);
-console.log(result);
