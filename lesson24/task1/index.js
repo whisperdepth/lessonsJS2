3855f0c0637e1e53d2a0dc6f863c1b3501d1febb
Roman Marchenko <whisperdepth@gmail.com>
2020-04-28 21:04:23 +0300

    v

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,5 +1,7 @@
 export const dayOfWeek = (date, days) => {
-  const week = ["Mo", "Tu", "We", "Th", "Fr", "St", "Su"];
+  const weekDays = ["Mo", "Tu", "We", "Th", "Fr", "St", "Su"];
   const dateInFuture = new Date(date).setDate(new Date(date).getDate() + days);
-  return week[new Date(dateInFuture).getDay()];
+  return weekDays[new Date(dateInFuture).getDay()];
 };
+
+
