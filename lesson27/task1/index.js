955fff657626f772cec7c8426c2b08c93efc50a4
Roman Marchenko <whisperdepth@gmail.com>
2020-05-04 13:48:37 +0300

    f

--- lesson27/task1/index.js
+++ lesson27/task1/index.js
@@ -1,1 +1,15 @@
 /* localStorage.clear(); */
+export const localStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;  
+    try{
+        newValue = JSON.parse(value);
+      }catch(){
+        newValue = value;
+      }
+    return {
+      ...acc,
+      [key]: newValue,
+    };
+  }, {});
+};
