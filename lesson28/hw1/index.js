3cab26ee60a90e384ba93e70a77113bae3a8dc1c
Roman Marchenko <whisperdepth@gmail.com>
2020-05-08 17:08:26 +0300

    e

--- lesson28/hw1/index.js
+++ lesson28/hw1/index.js
@@ -1,109 +1,103 @@
 export const shmoment = (date) => {
+  let newDate = date;
+
   const changer = {
-    add(timeUnits, value) {
+    add(value) {
       if (timeUnits == "years") {
-        const newDate = new Date(date).setFullYear(
+        newDate = new Date(date).setFullYear(
           new Date(date).getFullYear() + value
         );
-        return newDate;
+        return this;
       }
 
       if (timeUnits == "month") {
-        const newDate = new Date(date).setMonth(
-          new Date(date).getMonth() + value
-        );
-        return newDate;
+        newDate = new Date(date).setMonth(new Date(date).getMonth() + value);
+        return this;
       }
 
       if (timeUnits == "days") {
-        const newDate = new Date(date).setDate(
-          new Date(date).getDate() + value
-        );
-        return newDate;
+        newDate = new Date(date).setDate(new Date(date).getDate() + value);
+        return this;
       }
 
       if (timeUnits == "hours") {
-        const newDate = new Date(date).setHours(
-          new Date(date).getHours() + value
-        );
-        return newDate;
+        newDate = new Date(date).setHours(new Date(date).getHours() + value);
+        return this;
       }
 
       if (timeUnits == "minutes") {
-        const newDate = new Date(date).setMinutes(
+        newDate = new Date(date).setMinutes(
           new Date(date).getMinutes() + value
         );
-        return newDate;
+        return this;
       }
 
       if (timeUnits == "seconds") {
-        const newDate = new Date(date).setSeconds(
+        newDate = new Date(date).setSeconds(
           new Date(date).getSeconds() + value
         );
-        return newDate;
+        return this;
       }
 
       if (timeUnits == "milliseconds") {
-        const newDate = new Date(date).setMilliseconds(
+        newDate = new Date(date).setMilliseconds(
           new Date(date).getMilliseconds() + value
         );
-        return newDate;
+        return this;
       }
     },
 
     subtract(timeUnits, value) {
       if (timeUnits == "years") {
-        const newDate = new Date(date).setFullYear(
+        newDate = new Date(date).setFullYear(
           new Date(date).getFullYear() - value
         );
-        return newDate;
+        return this;
       }
 
       if (timeUnits == "month") {
-        const newDate = new Date(date).setMonth(
-          new Date(date).getMonth() - value
-        );
-        return newDate;
+        newDate = new Date(date).setMonth(new Date(date).getMonth() - value);
+        return this;
       }
 
       if (timeUnits == "days") {
-        const newDate = new Date(date).setDate(
-          new Date(date).getDate() - value
-        );
-        return newDate;
+        newDate = new Date(date).setDate(new Date(date).getDate() - value);
+        return this;
       }
 
       if (timeUnits == "hours") {
-        const newDate = new Date(date).setHours(
-          new Date(date).getHours() - value
-        );
-        return newDate;
+        newDate = new Date(date).setHours(new Date(date).getHours() - value);
+        return this;
       }
 
       if (timeUnits == "minutes") {
-        const newDate = new Date(date).setMinutes(
+        newDate = new Date(date).setMinutes(
           new Date(date).getMinutes() - value
         );
-        return newDate;
+        return this;
       }
 
       if (timeUnits == "seconds") {
-        const newDate = new Date(date).setSeconds(
+        newDate = new Date(date).setSeconds(
           new Date(date).getSeconds() - value
         );
-        return newDate;
+        return this;
       }
 
       if (timeUnits == "milliseconds") {
-        const newDate = new Date(date).setMilliseconds(
+        newDate = new Date(date).setMilliseconds(
           new Date(date).getMilliseconds() - value
         );
-        return newDate;
+        return this;
       }
     },
+
+    result(){
+        return newDate
+    }
   };
 
   return changer;
 };
 
-console.log( new Date(shmoment(new Date()).subtract("years", 1)));
+console.log(new Date(shmoment(new Date()).subtract("years", 1).result()));
