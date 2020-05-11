ec9774157db7b83c0b1a8f3818094f35f661d989
Roman Marchenko <whisperdepth@gmail.com>
2020-05-11 12:38:26 +0300

    d

--- lesson29/task3/index.js
+++ lesson29/task3/index.js
@@ -1,11 +1,11 @@
 export const printing = () => {
   console.log(1);
   setTimeout(function () {
-    console.log(4);
+    console.log(5);
   }, 1000);
   console.log(2);
   setTimeout(function () {
-    console.log(5);
+    console.log(4);
   }, 0);
   console.log(3);
 };
