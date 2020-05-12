3b64210f86139bd5ac74ff8841d0df28c02ac854
Roman Marchenko <whisperdepth@gmail.com>
2020-05-12 18:04:48 +0300

    ;

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -1,4 +1,4 @@
-export const addImageWithPromise = (imgSrc) => {
+export const addImage = (imgSrc) => {
   const p = new Promise((resolve, reject) => {
     const imgElem = document.createElement("img");
     imgElem.setAttribute("alt", "My photo");
