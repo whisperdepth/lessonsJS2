7466e3bd00c8a20fc48f499ddf69276a76d19550
Roman Marchenko <whisperdepth@gmail.com>
2020-05-14 16:24:44 +0300

    d

--- lesson31/task2/index.js
+++ lesson31/task2/index.js
@@ -4,18 +4,18 @@ export const asyncCalculator = (num) =>
       console.log(`Initial value: ${num}`);
       resolve(num);
     }, 500);
-  }).then(
-    (value) =>
-      new Promise((resolve) => {
-        setTimeout(() => {
-          const result = value * value;
-          console.log(`Squared value: ${result}`);
-          resolve(result);
-        }, 500);
-      })
-  ).then(value => {
+  })
+    .then(
+      (value) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            const result = value * value;
+            console.log(`Squared value: ${result}`);
+            resolve(result);
+          }, 500);
+        })
+    )
+    .then((value) => {
       const result = value * 2;
       console.log(`Doubled value: ${result}`);
-  })
-
-  asyncCalculator(5);
+    });
