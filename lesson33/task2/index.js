4e408db8035abc35fd54e21501fea2681e230de5
Roman Marchenko <whisperdepth@gmail.com>
2020-05-18 15:05:31 +0300

    d

--- lesson33/task2/index.js
+++ lesson33/task2/index.js
@@ -1,11 +1,11 @@
 const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks";
 
-const getTasksList = () => 
+export const getTasksList = () =>
   fetch(baseUrl).then((response) => response.json());
 
 
 
-const getTaskById = (taskId) =>
+export const getTaskById = (taskId) =>
   getTasksList().then((tasks) => tasks.find((task) => task.id == taskId));
 
-/* console.log(getTaskById(2)); */
+console.log(getTaskById(2));
