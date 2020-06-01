21c54067adbea84645e779d997503aeeeade1b16
Roman Marchenko <whisperdepth@gmail.com>
2020-06-01 16:48:04 +0300

    d

---
+++ lesson36/hw1/index.js
@@ -0,0 +1,17 @@
+export const getUsersBlogs = (usersArray) => {
+  const getUser = async (userId) => {
+    const user = await fetch(
+      `https://api.github.com/users/${userId}`
+    ).then((result) => result.json());
+    return user;
+  };
+
+  let promisesArray = [];
+  for (let i = 0; i < usersArray.length; i++) {
+    promisesArray.push(getUser(usersArray[i]));
+  }
+
+  return  Promise.all(promisesArray).then(responses => responses.map(response => response.blog))
+};
+
+console.log(getUsersBlogs(["google", "facebook", "gaearon"]));
