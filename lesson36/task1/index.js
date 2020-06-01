9360cd64a5a250433f6ea1c1fe926787e61aa335
Roman Marchenko <whisperdepth@gmail.com>
2020-06-01 14:10:31 +0300

    f

--- lesson36/task1/index.js
+++ lesson36/task1/index.js
@@ -1,11 +1,11 @@
 export const fetchUser = async (userName) => {
   try {
-    const response = await fetch(`https//:api.github.com/users/${userName}`);
+    const response = await fetch(`https://api.github.com/users/${userName}`);
     if (!response.ok) return null;
 
     const userData = await response.json();
     return userData;
-  }catch(e){
-      throw new Error("Failed to fetch user")
+  } catch (e) {
+    throw new Error("Failed to fetch user");
   }
 };
