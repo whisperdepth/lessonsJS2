bf34eef1f62c8c291eeeaba2ef113614ea58cdae
Roman Marchenko <whisperdepth@gmail.com>
2020-08-11 11:35:51 +0300

    в

---
+++ ClickDealer/addUser.js
@@ -0,0 +1,59 @@
+import { data, container, renderUsers, imgUrl, handleKeyUp } from "./index.js";
+let lastOnFocuse = null;
+const inputField = document.createElement("input");
+inputField.classList.add("input");
+const addButton = document.createElement("button");
+addButton.classList.add("btnA");
+addButton.textContent = "Add";
+
+const handleAddClick = () => {
+  if (!inputField.value) return;
+  data.accounts.push({
+    title: inputField.value,
+    image: imgUrl,
+    id: Math.random() + "",
+  });
+  inputField.value = null;
+  renderUsers(data.accounts);
+};
+addButton.addEventListener("click", handleAddClick);
+
+const cancelButton = document.createElement("button");
+cancelButton.classList.add("btnC");
+cancelButton.textContent = "Cancel";
+
+const handleCancelClick = () => {
+  inputField.value = null;
+  renderUsers(data.accounts);
+};
+
+cancelButton.addEventListener("click", handleCancelClick);
+
+const onBlur = (e) => {
+  lastOnFocuse = e.target;
+};
+
+export const onKeyUp = (e) => {
+  if (data.accounts.length === 0) return;
+  if (e.keyCode === 40) {
+    const toFocuseBtn = lastOnFocuse || document.querySelector(".btnA");
+
+    toFocuseBtn.focus();
+  } else if (e.keyCode === 38) {
+    document.querySelector(".input").focus();
+  } else if (e.keyCode === 39) {
+    document.querySelector(".btnC").focus();
+  } else if (e.keyCode == 37) {
+    document.querySelector(".btnA").focus();
+  }
+};
+
+addButton.addEventListener("blur", onBlur);
+cancelButton.addEventListener("blur", onBlur);
+
+export const renderAddUser = () => {
+  container.removeEventListener("keyup", handleKeyUp);
+  container.addEventListener("keyup", onKeyUp);
+  container.append(addButton, inputField, cancelButton);
+  document.querySelector(".input").focus();
+};
---
+++ ClickDealer/index.js
@@ -0,0 +1,99 @@
+import { renderAddUser, onKeyUp } from "./addUser.js";
+export const imgUrl = "https://avatars1.githubusercontent.com/u/9919?v=4";
+export const data = {
+  accounts: [
+    {
+      title: "user1",
+      image: imgUrl,
+      id: Math.random() + "",
+    },
+    {
+      title: "user2",
+      image: imgUrl,
+      id: Math.random() + "",
+    },
+    {
+      title: "user3",
+      image: imgUrl,
+      id: Math.random() + "",
+    },
+  ],
+};
+
+export const container = document.querySelector("#root");
+const listButton = document.createElement("button");
+listButton.classList.add("list-btn");
+listButton.textContent = "ADD";
+
+let lastOnFocuse = null;
+
+const handleClick = () => {
+  container.innerHTML = null;
+  renderAddUser();
+};
+listButton.addEventListener("click", handleClick);
+
+export const handleKeyUp = (e) => {
+  if (data.accounts.length === 0) return;
+  if (e.keyCode === 40 && document.activeElement.nextElementSibling) {
+    document.activeElement.nextElementSibling.focus();
+  } else if (
+    e.keyCode === 38 &&
+    document.activeElement.previousElementSibling
+  ) {
+    document.activeElement.previousElementSibling.focus();
+  } else if (e.keyCode === 39) {
+    document.querySelector(".list-btn").focus();
+  } else if (
+    e.keyCode === 37 &&
+    document.activeElement === document.querySelector(".list-btn")
+  ) {
+    lastOnFocuse.focus();
+  } else if (e.keyCode === 37) {
+    if (data.accounts.length < 2) return;
+    data.accounts = data.accounts.filter((user) => {
+      return user.id !== document.activeElement.dataset.id;
+    });
+
+    renderUsers(data.accounts);
+  }
+};
+
+const handleBlur = (e) => {
+  lastOnFocuse = e.target;
+};
+
+export const renderUsers = (accounts) => {
+  container.innerHTML = null;
+  container.removeEventListener("keyup", onKeyUp);
+  container.addEventListener("keyup", handleKeyUp);
+
+  const listItems = accounts.map((user) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("list-item");
+    listItem.tabIndex = 0;
+    listItem.addEventListener("blur", handleBlur);
+    listItem.dataset.id = user.id;
+
+    const userImg = document.createElement("img");
+    userImg.classList.add("img");
+    userImg.src = user.image;
+    userImg.alt = "user avatar";
+
+    const userTitle = document.createElement("span");
+    userTitle.textContent = user.title;
+
+    listItem.append(userImg, userTitle);
+    return listItem;
+  });
+
+  const usersList = document.createElement("ul");
+  usersList.classList.add("list");
+
+  usersList.append(...listItems);
+  container.append(usersList, listButton);
+
+  document.querySelector(".list-item").focus();
+};
+
+renderUsers(data.accounts);
